c1e84067508355785bc43af3d14a94b6c672e8d0
JKRUKA <harleyjia123@g.ucla.edu>
2025-03-06 02:28:40 -0800

    Initial commit

---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public GameObject gameOverUI; 
+
+    public void GameOver()
+    {
+        gameOverUI.SetActive(true); 
+        Time.timeScale = 0f; 
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+    }
+}
