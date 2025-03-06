348da7aa588d455994cc4f12967461debdf392c9
RJ-06 <rahulj0506@gmail.com>
2025-03-06 14:14:29 -0800

    death effect

--- Assets/Scripts/DeathEffect.cs
+++ Assets/Scripts/DeathEffect.cs
@@ -4,14 +4,29 @@ public class DeathEffect : MonoBehaviour
 {
     Rigidbody2D rb;
     [SerializeField] float effectTiming;
+    private float scalePos;
+    [SerializeField] float rotationSpeed;
+    bool isDying = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
+    }
+
+    void FixedUpdate() 
+    {
+        if (isDying) 
+        {
+            transform.localScale *= (1 - Time.deltaTime/effectTiming);
+        }
     }
 
     public void FlyOutOfWindow() 
     {
+        rb.freezeRotation = true;
+        rb.angularVelocity = rotationSpeed;
+        isDying = false;
+        Destroy(gameObject, effectTiming);
     }
 }
