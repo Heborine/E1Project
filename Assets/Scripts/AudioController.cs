80e98a81f48a33a9da6f5d1a40c4789faab1cc98
Kelvinxu28 <Kelvinxu28@gmail.com>
2025-03-13 01:55:01 -0700

    Audio... again
    it works now. please dont break.

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -2,33 +2,47 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Audio;
+using System.Collections;
+using UnityEngine.Scripting.APIUpdating;
 
 
 public class AudioController : MonoBehaviour
 {
     [SerializeField] AudioSource animatedTable;
     [SerializeField] AudioSource tableBam;
-    
+    bool grounded = true;
+    void OnCollisionEnter(Collision collision)
+        {
+            grounded = true;
+            Debug.Log("onground");
+        }
 
+     void OnCollisionExit(Collision collision)
+        {
+            grounded = false;
+            Debug.Log("air");
+        }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.F)){
-            animatedTable.Play();
-        }
-        else{
+
+         if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.F)){
+            if (!animatedTable.isPlaying && grounded == true){
+                animatedTable.Play();
+         }
+         }
+         else{
             animatedTable.Stop();
-        }
+         }
+        
+        
     }
 
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        tableBam.Play();
-    }
+
 }
